aa5a5ade038d484b934796ef5e5086dd05360989
Guillaume Lazaro <guillaume.lazaro@gmail.com>
2020-08-20 11:40:05 +0200

    Component ajouté

--- src/components/Commands.vue
+++ src/components/Commands.vue
@@ -1,12 +1,61 @@
 <template>
-<div id="commands" class="container d-flex justify-content-center">
+<div id="commands">
+    <div class="row justify-content-between mb-5">
+        <div class="my-3">
+            <label class="mr-2">Filtrer par</label>
+            <select class="px-1">
+                <option>Etat</option>
+                <option>Store</option>
+            </select>
+        </div>
+        <div class="m-3">
+            <input class="mr-2" />
+            <button class="btn btn-primary"> Rechercher</button>
+        </div>
+    </div>
 
+    <div>
+        <div class="row align-items-center justify-content-between border-bottom mb-2">
+            <div class="col-4 p-0"><b>Numéro</b></div>
+            <div class="col-4 p-0"><b>Etat</b></div>
+            <div class="col-4"><b>Store</b></div>
+        </div>
+
+        <div class="command row align-items-center justify-content-between mb-3 border-bottom" @click="commandPage(1)">
+            <div class="col-4 row align-items-center">
+                <h5>Commande 10</h5>
+            </div>
+            <div class="col-4">
+                Prête
+            </div>
+            <div class="col-4">
+                Toulouse
+            </div>
+        </div>
+
+        <div class="command row align-items-center justify-content-between mb-3 border-bottom" @click="commandPage(2)">
+            <div class="col-4 row align-items-center">
+                <h5>Commande 1000</h5>
+            </div>
+            <div class="col-4">
+                Cloturée
+            </div>
+            <div class="col-4">
+                Bordeaux
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
 <script>
 export default {
-    name: 'Commands'
+    name: 'Commands',
+    methods : {
+        commandPage: function(id) {
+            this.$router.push(`/product/${id}`);
+        }
+    }
 }
 </script>
 
@@ -15,5 +64,8 @@ export default {
 img{
     width : 90px;
     height : 90px
+}
+.command {
+    cursor : pointer;
 }
 </style>
