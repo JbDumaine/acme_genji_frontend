2e7f67adb66688e2c3c768b981f48ed51ef34f69
Guillaume Lazaro <guillaume.lazaro@gmail.com>
2020-08-21 13:06:48 +0200

    Ajout de requêtes dans store et page products/id bindé

--- src/store/index.js
+++ src/store/index.js
@@ -29,6 +29,13 @@ export default new Vuex.Store({
         stores: null,
         categories: null,
         receptions: null,
+
+        command: null,
+        supplier: null,
+        product: null,
+        store: null,
+        category: null,
+        reception: null
     },
     mutations: {
 
@@ -58,23 +65,46 @@ export default new Vuex.Store({
         setCommands(state, term){
             state.commands = term;
         },
+        setCommand(state, term){
+            state.command = term;
+        },
+
         setProductsByCategories(state, term) {
             state.products = term;
         },
         setProductsBySuppliers(state, term) {
             state.products = term;
         },
+        setProduct(state, term) {
+            state.product = term;
+        },
+
         setSuppliers(state, term) {
             state.suppliers = term;
         },
+        setSupplier(state, term) {
+            state.supplier = term;
+        },
+
         setCategories(state, term) {
             state.categories = term;
         },
+        setCategory(state, term) {
+            state.category = term;
+        },
+
         setStores(state, term) {
             state.stores = term;
         },
+        setStore(state, term) {
+            state.store = term;
+        }, 
+
         setReceptions(state, term) {
             state.receptions = term;
+        },
+        setReception(state, term) {
+            state.reception = term;
         }
     },
 
@@ -132,6 +162,26 @@ export default new Vuex.Store({
                     console.log(error);
                 });
         },
+        getCommand(context, commandId) {
+            let myInit = {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${context.state.requestToken}`,
+                },
+                body: JSON.stringify({
+                    commandId: commandId
+                })
+            };
+            fetch(AppConst.API_URL + "command/" + commandId, myInit)
+                .then(result => result.json())
+                .then((json) => {
+                    context.commit('setCommand', json)
+                })
+                .catch((error) => {
+                    console.error(`Une erreur s'est produite`);
+                    console.log(error);
+                });
+        },
 
         getProductsByCategories(context) {
             let myInit = {
@@ -150,7 +200,6 @@ export default new Vuex.Store({
                     console.log(error);
                 });
         },
-
         getProductsBySuppliers(context) {
             let myInit = {
                 headers: {
@@ -162,6 +211,23 @@ export default new Vuex.Store({
                 .then(result => result.json())
                 .then((json) => {
                     context.commit('setProductsBySuppliers', json)
+                })
+                .catch((error) => {
+                    console.error(`Une erreur s'est produite`);
+                    console.log(error);
+                });
+        },
+        getProduct(context, productId) {
+            let myInit = {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${context.state.requestToken}`,
+                }
+            };
+            fetch(AppConst.API_URL + "products/" + productId, myInit)
+                .then(result => result.json())
+                .then((json) => {
+                    context.commit('setProduct', json)
                 })
                 .catch((error) => {
                     console.error(`Une erreur s'est produite`);
