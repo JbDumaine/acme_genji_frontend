13ecc1873b35e8d289328a37cb93256c41f529a2
Ogoxi Narivony <dev@ogoxi.com>
2020-08-21 03:40:39 +0200

    Création des formulaires pour le supplier
    Création des vues pour ajouter et modifier un supplier.

---
+++ src/views/UpdateSupplier.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="form-add-product-container">
+    <form action="">
+      <h2>Modification d'un store</h2>
+      <div class="form-group">
+        <label for="name-supplier">Nom du Fournisseur : </label>
+        <input type="text" name="name-supplier" id="name-supplier">
+      </div>
+      <div class="form-group">
+        <label for="adress-supplier">Adresse du fournisseur : </label>
+        <textarea name="adress-supplier" id="adress-supplier" rows="5" cols="33"></textarea>
+      </div>
+      <div class="form-group">
+        <label for="city-supplier">Ville: </label>
+        <select name="city-supplier" id="city-supplier">
+          <option value="">Choisissez une ville</option>
+          <option value="">Nom de ville 1</option>
+          <option value="">Nom de ville 2</option>
+          <option value="">Nom de ville 3</option>
+          <option value="">Nom de ville 4</option>
+          <option value="">Nom de ville 5</option>
+        </select>
+      </div>
+    </form>
+  </div>
+</template>
+
+
+
+<script>
+import Vue from 'vue'
+export default Vue.extend({
+  name: "updateSupplier"
+})
+</script>
+
+<style scoped>
+.form-add-product-container{
+  margin-left: 5%;
+}
+</style>
