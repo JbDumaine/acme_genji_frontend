41dc94c512010fb658543e18d4bf1488ec3ca8df
jbdumaine <jbdumaine@ecofinance.fr>
2020-08-21 19:52:19 +0200

    add working form add store with ajax select cities

--- src/views/AddStore.vue
+++ src/views/AddStore.vue
@@ -1,41 +1,92 @@
 <template>
-  <div class="form-add-product-container">
-    <form action="">
-      <h2>Ajout d'un store</h2>
-      <div class="form-group">
-        <label for="name-store">Nom du magasin : </label>
-        <input type="text" name="name-store" id="name-store">
-      </div>
-      <div class="form-group">
-        <label for="adress-store">Adresse du magasin : </label>
-        <textarea name="adress-store" id="adress-store" rows="5" cols="33"></textarea>
-      </div>
-      <div class="form-group">
-        <label for="city-store">Ville: </label>
-        <select name="city-store" id="city-store">
-          <option value="">Choisissez une ville</option>
-          <option value="">Nom de ville 1</option>
-          <option value="">Nom de ville 2</option>
-          <option value="">Nom de ville 3</option>
-          <option value="">Nom de ville 4</option>
-          <option value="">Nom de ville 5</option>
-        </select>
-      </div>
-    </form>
+  <div class="form-add-product-container container">
+    <h2>Ajout d'un store</h2>
+    <div class="form-group">
+      <label for="name-store">Nom du magasin :</label>
+      <input v-model="storeForm.name" type="text" name="name-store" id="name-store" />
+    </div>
+    <div class="form-group">
+      <label for="adress-store">Adresse du magasin :</label>
+      <textarea v-model="storeForm.address" name="adress-store" id="adress-store" rows="5" cols="33"></textarea>
+    </div>
+    <div class="form-group">
+      <label for="city-store">Ville:</label>
+      <multiselect
+        v-model="city_chose"
+        id="cities_select"
+        track_by="id"
+        label="name"
+        :options="store"
+        :searchable="true"
+        placeholder="Entrer une ville ou un code postal"
+        @search-change="getData"
+      ></multiselect>
+    </div>
+    <b-button v-on:click="addStore" variant="primary">Valider</b-button>
   </div>
 </template>
 
 
 
 <script>
-import Vue from 'vue'
+import Vue from "vue";
+import Multiselect from "vue-multiselect";
+import AppConst from "../appConst.js";
+Vue.component("multiselect", Multiselect);
 export default Vue.extend({
-  name: "addStore"
-})
+  name: "addStore",
+  components: {
+    Multiselect,
+  },
+  data() {
+    return {
+      isLoading: false,
+      value: null,
+      store: [],
+      queryData: null,
+      city_chose: null,
+      storeForm: {
+        name: null,
+        address: null,
+        city_id: null,
+      },
+    };
+  },
+  methods: {
+    getData(query) {
+      this.isLoading = true;
+      if (query.length > 0) {
+        this.queryData = JSON.parse(JSON.stringify(query));
+        fetch(AppConst.API_URL + `cities/search/${escape(query)}`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${this.$store.state.requestToken}`,
+          },
+        })
+          .then((result) => result.json())
+          .then((json) => {
+            let result = [];
+            json.forEach((element) => {
+              result.push(element);
+            });
+            console.log(result);
+            this.store = result;
+            this.isLoading = false;
+          })
+          .catch((error) => {
+            console.log(error);
+            this.isLoading = false;
+          });
+      }
+    },
+    addStore: function () {
+      console.log("coucou");
+      this.storeForm.city_id = this.city_chose.id;
+      this.$store.dispatch("addStore", this.storeForm);
+    },
+  },
+});
 </script>
-
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 <style scoped>
-.form-add-product-container{
-  margin-left: 5%;
-}
 </style>
