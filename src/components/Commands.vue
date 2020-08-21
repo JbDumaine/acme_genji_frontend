f223b49949ed086de3f016d53177612a21177dae
Guillaume Lazaro <guillaume.lazaro@gmail.com>
2020-08-21 23:59:43 +0200

    Commands vue update

--- src/components/Commands.vue
+++ src/components/Commands.vue
@@ -4,21 +4,29 @@
         <font-awesome-icon icon="plus-circle"/>
       </a>
     <div class="row align-items-center justify-content-between border-bottom mb-3">
-        <div class="col-4"><b>Numéro</b></div>
-        <div class="col-4"><b>Etat</b></div>
-        <div class="col-4"><b>Store</b></div>
+        <div class="col-2"><b>Numéro</b></div>
+        <div class="col-2"><b>Etat</b></div>
+        <div class="col-3"><b>Store</b></div>
+        <div class="col-3"><b>Date de création</b></div>
+        <div class="col-2"><b>Date de livraison</b></div>
     </div>
 
     <div v-for="command in this.$store.state.commands" :key="command.id">
         <div class="command row align-items-center justify-content-between mb-3 border-bottom" @click="commandPage(command.id)">
-            <div class="col-4">
+            <div class="col-2">
                 {{command.command_number}}
             </div>
-            <div class="col-4">
+            <div class="col-2">
                 {{command.state.name}}
             </div>
-            <div class="col-4">
+            <div class="col-3">
                 {{command.store.name}}
+            </div>
+            <div class="col-3">
+                {{command.created_at}}
+            </div>
+            <div class="col-2">
+                {{command.delivery_date}}
             </div>
         </div>
     </div>
